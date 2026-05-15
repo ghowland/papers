8842557cf738d320098cf09cfbfbc2ceb8f2027a
Geoffrey Howland <geoff@gmail.com>
2026-05-15 18:25:46 +0700

    Update All

--- papers/papers/VDR/HOWL-VDR-1-2026/code/vdr/__init__.py
+++ papers/papers/VDR/HOWL-VDR-1-2026/code/vdr/__init__.py
@@ -11,3 +11,8 @@ Exact finite discrete representation.
 from vdr.vdr import VDR, Remainder, VDRError
 from vdr.linalg import Vec, Mat, LinAlgError, parse_vdr, vdr_to_dict, vdr_from_dict, vdr_to_latex
 from vdr.export import to_decimal, to_float, to_fraction
+
+# Auto-install active multiplication/division
+import vdr.active_mul
+vdr.active_mul.install()
+
